a968436203a555681c648a41d1ed030e9e5c4fab
xsatrio <satriocahyo93@gmail.com>
2025-04-15 21:58:57 +0700

    testing module2.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,14 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
+        jvmTarget = "11"
     }
 }
 
